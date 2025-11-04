92254828d6efc0860f8007ba8b7fe02364ac42a0
Fabricio M Damasceno <fabricio.damasceno@proton.me>
2025-11-03 23:50:09 -0300

    feat(filament): Enable user registration and password reset

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->passwordReset()
+            ->registration()
             ->colors([
                 'primary' => Color::Amber,
             ])
